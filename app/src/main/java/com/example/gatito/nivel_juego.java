7c2002bc5b61a6dee3025e0fec11b0f961fe6343
Carina Amairani Díaz Ramírez <1717110615@utectulancingo.edu.mx>
2019-03-25 10:04:32 -0600

    Agregando linea para quitar el nombre del activity

--- app/src/main/java/com/example/gatito/nivel_juego.java
+++ app/src/main/java/com/example/gatito/nivel_juego.java
@@ -9,5 +9,6 @@ public class nivel_juego extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nivel_juego);
+        getSupportActionBar().hide();
     }
 }
